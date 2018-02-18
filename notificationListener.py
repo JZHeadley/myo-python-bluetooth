7049f10fa27c6fb887153afbfb2d544c488de85b
John Naylor <jonaylor89@gmail.com>
2018-02-18 09:58:31 +0000

    fixed my stupid loop

--- notificationListener.py
+++ notificationListener.py
@@ -16,7 +16,8 @@ def main():
 	SERVER_IP = "172.29.61.75"
 	
 	per.connect("EF:CD:C9:EA:16:6C")
-	while per.addr is not None:
+	print(per.addr)
+	while per.addr is None:
 		per.connect("EF:CD:C9:EA:16:6C")
 	
 	def print_stat(msg):
